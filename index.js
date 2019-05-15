afa8cfbce38621e405f272f06e02112498a7cb46
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-15 18:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function add(x,y){
   return x+y;
 }
+function subtract(x,y){
+  return x-y;
+}
+function multiply(x,y){
+  return x*y;
+}
+function divide(x,y){
+  return x/y;
+}
+function increment(x){
+  return x++;
+}
+function decrement(x){
+  return x--;
+}
